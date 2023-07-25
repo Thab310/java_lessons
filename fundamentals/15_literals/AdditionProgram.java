80c60bee1eb0759699ff547f478b1cf1a472b743
Thabelo <thabelo@commandquality.co.za>
2023-07-25 07:56:50 +0200

    finished literals

---
+++ fundamentals/15_literals/AdditionProgram.java
@@ -0,0 +1,10 @@
+public class AdditionProgram {
+    public static void main(String[] args) {
+        int operand1 = 10;
+        int operand2 = 20;
+
+        int result = operand1 + operand2;
+
+        System.out.println("The result of the addition is: " + result);
+    }
+}
