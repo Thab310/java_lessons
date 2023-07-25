5f980746a7bbdb8e939100df2c1c6e470b52e31b
Thabelo <thabelo@commandquality.co.za>
2023-07-25 07:21:15 +0200

    finished keyword

---
+++ fundamentals/13_keywords/Keyword.java
@@ -0,0 +1,9 @@
+public class Keyword{
+	public static void main(String[] args){
+		String name = "Thabelo";
+		String greeting = "Hello " + name;
+		if (name.equals("Thabelo")){
+			System.out.println(greeting);
+		}
+	} 
+}
